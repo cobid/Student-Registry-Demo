4802a827e7728732a08f6aadb183bb10809e1e3b
Ivaylo Gatev <93418396+cobid@users.noreply.github.com>
2024-04-11 14:24:57 +0300

    change jenkins test  files

--- tests/view-students.tests.js
+++ tests/view-students.tests.js
@@ -2,11 +2,11 @@ const assert = require('assert');
 const fetch = require('node-fetch');
 
 suite('View Students page', function() {
-  test('Page title', async function() {
-    let res = await fetch("http://localhost:8888/students");
-    let body = await res.text();
-    assert.ok(body.includes("<h1>Registered Students</h1>"));
-  });
+  test('Page title', async () => {
+      let res = await fetch("http://localhost:8888/students");
+      let body = await res.text();
+      assert.ok(body.includes("<h1>Registered Students</h1>"));
+    });
   
   // test('Students list', async function() {
   //   let res = await fetch("http://localhost:8888/students");
